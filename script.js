69abf67d8b0dfbcee27a2d43449de7265282f3de
EverRak <pedromontanari09@gmail.com>
2026-05-21 14:52:28 -0300

    Ódio pela tecnologia
    Alterações consertadas (talvez não 100%, mas feitas)
    Agora o logout funfa legalzinho (realmente faz logout)

--- script.js
+++ script.js
@@ -17,3 +17,21 @@ function exibirModal(titulo, mensagem, urlDestino = null, campoFocus = null) {
 
     modal.style.display = 'flex';
 }
+
+function fazerLogout() {
+    localStorage.removeItem('usuarioLogado');
+    exibirModal("Informação", "Logout realizado com sucesso.", "login.html");
+    console.log("logout feito, fi");
+}
+
+function calcularSugestao(imc) {
+    let sugestao = "";
+    if (imc < 18.5) {
+        sugestao = "Ganhar massa";
+    } else if (imc < 25) {
+        sugestao = "Manter alimentação saudável";
+    } else {
+        sugestao = "Emagrecer";
+    }
+    return sugestao;
+}
