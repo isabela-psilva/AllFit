1268e034976edfcd5da810ca33f81b1c0e14055f
isabela-psilva <bela.pereira2025@gmail.com>
2026-09-21 11:36:50 -0300

    Atualização do PHP
    fiz mais uma tabela (perfis) e linkei com a pagina de criar perfil e dashboard e esta funcionando. assim q se cria uma conta, ela loga, vc vai pro dashboard e tem a opção de criar o perfil caso sua conta nao esteja armazenada no bd, caso esteja, ele já mostra o imc certinho. 
    proxima coisa a se fazer é:
    Banco: usar o banco allfit.
    Criar a tabela objetivos com:
    id_objetivo → INT, PRIMARY KEY, AUTO_INCREMENT
    nome → VARCHAR(100), NOT NULL
    Criar a tabela usuario_objetivo com:
    id_usuario_objetivo → INT, PRIMARY KEY, AUTO_INCREMENT
    id_usuario → INT
    id_objetivo → INT
    
    assim q criar, linkar com o dashboard pfv

---
+++ AllFit/conexao.php
@@ -0,0 +1,8 @@
+<?php
+$servidor = "127.0.0.1";
+$usuario = "root";
+$senha = "usbw";
+$banco = "allfit";
+$conecta_db = mysql_connect($servidor, $usuario, $senha) or die (mysql_error());
+mysql_select_db($banco) or die ("Erro ao Conectar");
+?>
---
+++ AllFit/gravar_perfil.php
@@ -0,0 +1,48 @@
+<?php 
+    session_start();
+
+    include 'conexao.php';
+
+    $id_usuario = $_SESSION["id_usuario"];
+
+    $idade = $_POST["txt_idade"];
+    $altura = $_POST["txt_altura"];
+    $peso = $_POST["txt_peso"];
+
+    if ($idade == "" || $altura == "" || $peso == "") {
+        header("Location: perfil.php?titulo=Atenção!&mensagem=Preencha todos os campos!");
+        exit;
+    }
+
+    $sql = mysql_query("Select * from perfis
+                        Where id_usuario = '$id_usuario'");
+
+    if (!$sql) {
+        die("Erro na consulta: " . mysql_error());
+    }
+
+    if (mysql_num_rows($sql) > 0) {
+        $sql = mysql_query("Update perfis
+                            Set idade = '$idade',
+                                altura = '$altura',
+                                peso = '$peso'
+                            Where id_usuario = '$id_usuario'");
+        
+        if (!$sql) {
+            die("Erro ao atualizar o perfil: " . mysql_error());
+        }
+    }
+    else {
+        $sql = mysql_query("Insert Into perfis
+                            (id_usuario, idade, altura, peso)
+                            Values
+                            ('$id_usuario', '$idade', '$altura', '$peso')");
+            
+        if (!$sql) {
+            die("Erro ao salvar o perfil: " . mysql_error());
+        }                    
+    }
+
+    header("Location: dashboard.php?titulo=Sucesso!&mensagem=Perfil salvo com sucesso!");
+    exit;
+?>
